ec0f940640d2a4b36f8be5179914c2b58b40fc6e
Zander Martineau <zander@martineau.tv>
2018-12-21 21:55:58 +0000

    change to zander

--- build.js
+++ build.js
@@ -1,32 +1,24 @@
 'use strict'
 
-// Pull in our modules
 const chalk = require('chalk')
 const boxen = require('boxen')
 const fs = require('fs')
 const path = require('path')
 
-// Define options for Boxen
-const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round'
-}
-
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Zander Martineau /'),
+  handle: chalk.cyan('MrMartineau'),
+  work: chalk.white('Freelance front-end developer'),
+  twitter: chalk.cyan('https://twitter.com/mrmartineau'),
+  github: chalk.cyan('https://github.com/mrmartineau'),
+  mastodon: chalk.cyan('https://toot.cafe/@zander'),
+  web: chalk.cyan('https://zander.wtf'),
+  npx: chalk.white('npx mrmartineau'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelMastodon: chalk.white.bold('  Mastodon:'),
   labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
@@ -37,11 +29,16 @@ const heading = `${data.name} ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const mastodoning = `${data.labelMastodon}  ${data.mastodon}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working + newline + githubing + newline + mastodoning + newline + webing + newline + twittering + newline + newline + carding
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'double',
+  borderColor: 'white'
+})))
